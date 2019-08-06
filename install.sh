13b9bd2e878389bb51e9284408e21da70407dfd1
Frederico Cabral <frederico.cabral@getyourguide.com>
2019-08-06 15:58:39 +0200

    Install VIM

--- install.sh
+++ install.sh
@@ -6,13 +6,20 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' #NO COLOR
 
-main() {
-  check 'Docker' $(check_docker)
-}
 
 setup_vim() {
   mv ~/.vimrc ~/.vimrc_old
-  ln -s vim/vimrc ~/.vimrc
+  ln -s ~/work/miseenplace/vim/vimrc ~/.vimrc
+}
+
+main() {
+  setup 'VIM' $(setup_vim)
+}
+
+setup() {
+  echo -n "$1: "
+  $2 &> /dev/null
+  [[ $? -eq 0 ]] && echo $(ok) || echo $(fail)
 }
 
 # --------------------------------------------------------------------------
@@ -27,17 +34,6 @@ ok() {
 # --------------------------------------------------------------------------
 fail() {
   echo -e "${RED}[FAIL]${NC}"
-}
-
-# --------------------------------------------------------------------------
-# DESC: Check if a given software/tool is installed
-# --------------------------------------------------------------------------
-# $1 -> Description will be printed
-# $2 -> Checker function
-check() {
-  echo -n "$1: "
-  $2 &> /dev/null
-  [[ $? -eq 0 ]] && echo $(ok) || echo $(fail)
 }
 
 main
