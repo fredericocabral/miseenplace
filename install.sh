b19dcabffb24d8e9094a056b62dd184257d2224c
Frederico Cabral <fredericocabral@Fredericos-MacBook-Pro.local>
2025-11-07 12:45:44 +0100

    basic install script

---
+++ install.sh
@@ -0,0 +1,62 @@
+
+
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m" # No Color
+
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "${GREEN}oh-my-zsh is installed${NC}"
+else
+  echo "oh-my-zsh is not installed"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  ln -s zsh/zshrc ~/.zshrc
+fi
+
+
+if command -v nvim >/dev/null 2>&1; then
+  echo "${GREEN}NEOVIM is installed${NC}"
+else
+  echo "${RED}Neovim is not installed${NC}"
+  read -p "Do you want to install Neovim? (y/n): " choice
+  case "$choice" in
+    y|Y )
+      echo "Installing NEOVIM"
+      brew install neovim
+      ;;
+    * )
+      echo "Skipping installation."
+      ;;
+   esac
+fi
+
+
+if command -v tmux >/dev/null 2>&1; then
+  echo "${GREEN}TMUX is installed${NC}"
+else
+  echo "${RED}TMUX is not installed${NC}"
+  read -p "Do you want to install TMUX? (y/n): " choice
+  case "$choice" in
+    y|Y )
+      echo "Installing TMUX"
+      brew install tmux
+      ;;
+    * )
+      echo "Skipping installation."
+      ;;
+   esac
+fi
+
+## mv ~/.zshrc ~/.zshrc.old
+ln -s "$(pwd)/zsh/zshrc" ~/.zshrc
+ln -s "$(pwd)/tmux/tmux.conf" ~/.tmux.conf
+ln -s "$(pwd)/nvim" ~/.config/nvim
+touch ~/.variables.env
+
+
+
+
+
+
+
+
